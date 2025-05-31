636efd6a17967efaed0c61ad572340d595cdbc23
Mohamed Mousa <77318165+mohamedmousa77@users.noreply.github.com>
2025-06-01 00:58:58 +0200

    Book Management
    Book API created using ASP.NET, that would be managed and called by angular.

---
+++ BookApi/BookApi/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using BookApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+namespace BookApi.Controllers
+{
+    // https://localhost:7033/api/books
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class BooksController : ControllerBase
+    {
+        private Book[] _book = new Book[]
+        {
+            new Book { Id = 1, Title = "1984", Author = "George Orwell" },
+            new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee" },
+            new Book { Id = 3, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return Ok(_book);
+        }
+    }
+}
